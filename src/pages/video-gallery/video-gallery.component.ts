be19c63247290a6d5409fe401d225b872dc7fb12
Ivan Safiulin <ivansafiulin1997@gmail.com>
2022-01-14 02:10:14 +0200

    Education. Build project. Minor

--- src/pages/video-gallery/video-gallery.component.ts
+++ src/pages/video-gallery/video-gallery.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IVideo } from '../models/interfaces/video.interface';
 import { videos } from '../../shared/data/video.data';
-import { fadeInAnimation } from '../../animations/fade-in-animation.animation';
 
 @Component({
   selector: 'app-video-gallery',
@@ -10,12 +9,13 @@ import { fadeInAnimation } from '../../animations/fade-in-animation.animation';
 })
 export class VideoGalleryComponent implements OnInit {
 
-  videos: IVideo[] = JSON.parse(JSON.stringify(videos));
+  videos: IVideo[] =  JSON.parse(JSON.stringify(videos));
 
   constructor() {
   }
 
   ngOnInit(): void {
+
   }
 
   identify(index: number, item: IVideo) {
